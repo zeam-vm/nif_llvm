fe619cb3bbf315b13185fead23f2a2b60750c85c
zacky1972 <zacky1972@gmail.com>
2018-09-22 14:27:40 +0900

    use Asm

--- lib/nif_llvm.ex
+++ lib/nif_llvm.ex
@@ -1,12 +1,15 @@
 defmodule NifLlvm do
+  require Asm
+  import Asm
+
+  @max_int 9_223_372_036_854_775_807
+  @min_int -9_223_372_036_854_775_808
+
   @on_load :load_nifs
 
   def load_nifs do
     :erlang.load_nif('./priv/libnifllvm', 0)
   end
-
-  @max_int 9_223_372_036_854_775_807
-  @min_int -9_223_372_036_854_775_808
 
   @moduledoc """
   Documentation for NifLlvm.
@@ -33,22 +36,21 @@ defmodule NifLlvm do
 
   def asm_1(a, b) do
     case {a, b} do
-        {a, b} when is_integer(a) and a <= @max_int and a >=@min_int
-          and is_integer(b) and b <= @max_int and b >=@min_int
+        {a, b} when is_int64(a) and is_int64(b)
           -> case asm_1_nif_ii(a, b) do
             x when is_integer(x) -> x
             :error -> raise ArithmeticError, message: "bad argument in arithmetic expression"
           end
-        {a, b} when is_integer(a) and a <= @max_int and a >=@min_int and is_float(b) -> asm_1_nif_if(a, b)
-        {a, b} when is_float(a) and is_integer(b) and b <= @max_int and b >=@min_int -> asm_1_nif_fi(a, b)
+        {a, b} when is_int64(a) and is_float(b) -> asm_1_nif_if(a, b)
+        {a, b} when is_float(a) and is_int64(b) -> asm_1_nif_fi(a, b)
         {a, b} when is_float(a) and is_float(b) -> asm_1_nif_ff(a, b)
         _ -> raise ArithmeticError, message: "bad argument in arithmetic expression"
     end
   end
 
-  def asm_1_nif_ii(a, b) when is_integer(a) and is_integer(b), do: raise "NIF asm_1_nif_ii/2 not implemented"
-  def asm_1_nif_if(a, b) when is_integer(a) and is_float(b),   do: raise "NIF asm_1_nif_if/2 not implemented"
-  def asm_1_nif_fi(a, b) when is_float(a)   and is_integer(b), do: raise "NIF asm_1_nif_fi/2 not implemented"
-  def asm_1_nif_ff(a, b) when is_float(a)   and is_float(b),   do: raise "NIF asm_1_nif_ff/2 not implemented"
+  def asm_1_nif_ii(a, b) when is_int64(a) and is_int64(b), do: raise "NIF asm_1_nif_ii/2 not implemented"
+  def asm_1_nif_if(a, b) when is_int64(a) and is_float(b), do: raise "NIF asm_1_nif_if/2 not implemented"
+  def asm_1_nif_fi(a, b) when is_float(a) and is_int64(b), do: raise "NIF asm_1_nif_fi/2 not implemented"
+  def asm_1_nif_ff(a, b) when is_float(a) and is_float(b), do: raise "NIF asm_1_nif_ff/2 not implemented"
 
 end
